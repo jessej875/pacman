72939e72f08fe670646c9d94420d6196ae44e289
jessejiang <jessej1020@gmail.com>
2025-12-28 01:41:18 -0500

    Updated view class and added render method to know which view (game or menu) to render

--- src/main/java/com/pacman/view/View.java
+++ src/main/java/com/pacman/view/View.java
@@ -1,3 +1,38 @@
 package com.pacman.view;
 
-public class View {}
+import com.pacman.controller.Controller;
+import com.pacman.model.Game;
+import com.pacman.model.Observer;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View implements FXComponent, Observer {
+    private final Controller controller;
+    private final Game game;
+    private final Stage stage;
+
+    public View(Controller controller, Game game, Stage stage) {
+        this.controller = controller;
+        this.game = game;
+        this.stage = stage;
+    }
+
+    @Override
+    public Parent render() {
+        if (game.isPlaying() || game.isGameOver() || game.isLevelComplete()) {
+            GameView gameView = new GameView(controller, game);
+            return gameView.render();
+        } else {
+            MenuView menuView = new MenuView(controller, game);
+            return menuView.render();
+        }
+    }
+
+    @Override
+    public void update() {
+        Scene scene = new Scene(render(), 800, 900);
+        scene.getStylesheets().add("pacman.css");
+        stage.setScene(scene);
+    }
+}
