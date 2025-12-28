6ba1981b24b5c0556cfdfeeeaf976a6827e2816d
jessejiang <jessej1020@gmail.com>
2025-12-27 20:56:59 -0500

    Created class entity to delegate behavior to subclasses and created class position to get entity position

--- src/main/java/com/pacman/model/Entity.java
+++ src/main/java/com/pacman/model/Entity.java
@@ -1,3 +1,24 @@
 package com.pacman.model;
 
-public class Entity {}
+public abstract class Entity {
+  protected Position position;
+  protected String imagePath;
+
+  public Entity(int row, int col, String imagePath) {
+    this.position = new Position(row, col);
+    this.imagePath = imagePath;
+  }
+
+  public Position getPosition() {
+    return position;
+  }
+
+  public String getImagePath() {
+    return imagePath;
+  }
+
+  public void setPosition(int row, int col) {
+    position.setRow(row);
+    position.setCol(col);
+  }
+}
---
+++ src/main/java/com/pacman/model/Position.java
@@ -0,0 +1,31 @@
+package com.pacman.model;
+
+public class Position {
+  private int row;
+  private int col;
+
+  public Position(int row, int col) {
+    this.row = row;
+    this.col = col;
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public int getCol() {
+    return col;
+  }
+
+  public void setRow(int row) {
+    this.row = row;
+  }
+
+  public void setCol(int col) {
+    this.col = col;
+  }
+
+  public boolean equals(Position other) {
+    return this.row == other.row && this.col == other.col;
+  }
+}
